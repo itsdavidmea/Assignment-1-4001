138a153ef45cebcf7e00be19fc9d6786bb8f87ba
David Mea <mea.david29@outlook.com>
2025-10-05 18:02:36 -0400

    added simulation for CPU

--- interrupts.cpp
+++ interrupts.cpp
@@ -13,25 +13,36 @@ int main(int argc, char** argv) {
     //delays  is a C++ std::vector of ints that contain the delays of each device
     //the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
-    std::ifstream input_file(argv[1]);
+    std::ifstream input_file(argv[1]); //ifstream is used to read from files and it's part of the standard library
 
     std::string trace;      //!< string to store single line of trace file
     std::string execution;  //!< string to accumulate the execution output
-
+    
     /******************ADD YOUR VARIABLES HERE*************************/
-
+    int mode_bit = 1;       // when 1 = user mode, when 2 = kernel mode 
+    int time_of_event = 0; 
+   
+    
 
 
     /******************************************************************/
 
     //parse each line of the input trace file
     while(std::getline(input_file, trace)) {
-        auto [activity, duration_intr] = parse_trace(trace);
+        execution = "";
+        auto [activity, duration_intr] = parse_trace(trace); //parse_trace will return a tuple: ('CPU', 50)
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity == "CPU") {
+            if (mode_bit == 1)
+            {
+                mode_bit = 0;
+            }
+            execution += std::to_string(time_of_event) + ", " + std::to_string(duration_intr) + ", " + "CPU burst";
+            
+        }
 
-
-
+        write_output(execution);
         /************************************************************************/
 
     }
